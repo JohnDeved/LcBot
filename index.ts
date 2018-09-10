9742a1e5113551dd360f8c576c972109277f290d
undefined <johann@objekt.stream>
2018-09-10 14:14:46 +0200

    + commands

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-import { Client } from 'discord.js'
+import { Client, Message } from 'discord.js'
 
 class LcBot {
   private client: Client = new Client()
@@ -12,18 +12,29 @@ class LcBot {
     return args
   }
 
+  private commands: object = {
+    test: (msg: Message) => {
+      msg.reply('hello world')
+    }
+  }
+
   private on: object = {
     ready: () => {
       console.log(`Logged in as ${this.client.user.tag}!`)
     },
-    message: msg => {
-      if (/^![\w_]+/) {
-        
+    message: (msg: Message) => {
+      const [, command ]: RegExpMatchArray = msg.content.match(/^!([\w_]+)/)
+      console.log(command)
+      if (command && this.commands[command]) {
+        const args = this.parseArgs(msg)
+        this.commands[command](msg, ...args)
+      } else {
+        command && msg.reply(`unknown command \`${command}\``)
       }
     }
   }
 
-  public init () {
+  public init (): void {
     for (const key in this.on) {
       if (this.on.hasOwnProperty(key)) {
         const handler = this.on[key]
