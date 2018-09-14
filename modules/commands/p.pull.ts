4b823a5ca49a1a6268782e9a183b8737ca5d065e
undefined <johann@objekt.stream>
2018-09-14 12:41:06 +0200

    + pull command

---
+++ modules/commands/p.pull.ts
@@ -0,0 +1,43 @@
+import * as git from 'simple-git'
+import { Message, RichEmbed } from 'discord.js'
+import { IcommandExport } from '..'
+
+export interface Itest {
+  (msg: Message, ...args: string[]): Promise<Message | Message[]>
+}
+
+const exp: IcommandExport = {
+  description: 'executes a git pull in the configured git repo',
+  handler: async (msg: Message) => {
+    if (!process.env.REPO) {
+      await msg.reply('dotenv var "REPO" was not defined!')
+      return
+    }
+
+    await git(process.env.REPO)
+      .pull((err, pull) => {
+        if (err) return console.error(err)
+        const embed = new RichEmbed()
+
+        for (const key in pull.summary) {
+          if (pull.summary.hasOwnProperty(key)) {
+            const element = pull.summary[key]
+            embed.addField(key, pull.summary[key])
+          }
+        }
+
+        msg.reply(embed)
+      })
+      .log(['-1'], (err, log) => {
+        if (err) return console.error(err)
+        const embed = new RichEmbed()
+
+        embed.setTitle(log.latest.author_name)
+        embed.addField(log.latest.hash.slice(0, 8) + ':', log.latest.message)
+
+        msg.reply('current commit:', { embed })
+      })
+  }
+}
+
+module.exports = exp
